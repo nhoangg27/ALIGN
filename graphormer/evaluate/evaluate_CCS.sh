0fdf72e81b44932c5fcc4084a83fb63a7df5af32
Nhi Hoang <81710816+nhoangg27@users.noreply.github.com>
2026-05-13 14:58:34 -0700

    Add evaluate_CCS.sh script for CCS evaluation

---
+++ graphormer/evaluate/evaluate_CCS.sh
@@ -0,0 +1,20 @@
+python evaluate_CCS.py \
+    --user-dir /workspaces/align/graphormer \
+    --num-workers 32 \
+    --ddp-backend=legacy_ddp \
+	--user-data-dir CCS_loader \
+	--dataset-name CCS_test \
+    --task graph_prediction \
+	--criterion rmse \
+	--arch graphormer_base \
+    --encoder-layers 8 \
+    --encoder-embed-dim  512 \
+    --encoder-ffn-embed-dim 512 \
+    --encoder-attention-heads 128 \
+    --freeze-level -4 \
+    --mlp-layers 5 \
+    --batch-size 64 \
+    --num-classes 1 \
+    --save-dir /workspaces/align/checkpoints/checkpoint_last.pt \
+    # --save-path /workspaces/align/results/CCS_sample.csv \
+    --split train \
