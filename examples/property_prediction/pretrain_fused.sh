cf9834fa171f86a26d065e6863df99c6d2aea96c
Nhi Hoang <81710816+nhoangg27@users.noreply.github.com>
2026-05-13 13:48:57 -0700

    Create pretrain_fused.sh

---
+++ examples/property_prediction/pretrain_fused.sh
@@ -0,0 +1,28 @@
+CUDA_VISIBLE_DEVICES=0 fairseq-train \
+	--user-dir /workspaces/align/graphormer \
+	--batch-size 64 \
+	--num-workers 20 \
+	--ddp-backend=legacy_ddp \
+	--seed 23 \
+	--user-data-dir fused_pretrain \
+	--dataset-name fused_rt_library \
+	--task graph_prediction_with_flag \
+	--criterion rmse \
+	--arch graphormer_base \
+	--num-classes 1 \
+	--attention-dropout 0.15 --act-dropout 0.10 --dropout 0.10 \
+	--optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.01 \
+	--lr-scheduler polynomial_decay --power 1 --warmup-updates 92039 --total-num-update 613594 \
+	--lr 1e-4 \
+	--fp16 \
+  --encoder-layers 8 \
+  --encoder-embed-dim  512 \
+  -encoder-ffn-embed-dim 512 \
+  --encoder-attention-heads 128 \
+  --mlp-layers 5 \
+	--max-epoch 250 \
+	--no-epoch-checkpoints \
+	--freeze-level 0 \
+	--save-dir /workspaces/align/checkpoints \
+
+
