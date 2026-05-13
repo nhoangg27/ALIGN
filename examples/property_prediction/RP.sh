e1f3b6bda69a77d9185b384a81f9b1d083537546
Nhi Hoang <81710816+nhoangg27@users.noreply.github.com>
2026-05-13 12:24:15 -0700

    Create RP.sh

---
+++ examples/property_prediction/RP.sh
@@ -0,0 +1,31 @@
+CUDA_VISIBLE_DEVICES=0 fairseq-train \
+    --user-dir /workspaces/align/graphormer \
+    --batch-size 64 \
+    --num-workers 20 \
+    --ddp-backend=legacy_ddp \
+    --seed 23 \
+    --user-data-dir RP_loader \
+    --dataset-name RP \
+    --task graph_prediction_with_flag \
+    --criterion rmse \
+    --arch graphormer_base \
+    --num-classes 1 \
+    --attention-dropout 0.15 --act-dropout 0.10 --dropout 0.10 \
+    --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.01 \
+    --lr-scheduler polynomial_decay --power 1 --warmup-updates 48 --total-num-update 317 \
+    --lr 2e-4 \
+    --fp16 \
+    --encoder-layers 8 \
+    --encoder-embed-dim 512 \
+    --encoder-ffn-embed-dim 512 \
+    --encoder-attention-heads 64 \
+    --mlp-layers 5 \
+    --max-epoch 100 \
+    --no-epoch-checkpoints \
+    --freeze-level -4  \
+    --save-dir '/workspaces/align/checkpoints' \
+    --pretrained-model-name /workspaces/align/model_weights/RP_specialist/checkpoint_last.pt \
+	--finetune-from-model /workspaces/align/model_weights/RP_specialist/checkpoint_last.pt \
+
+
+
