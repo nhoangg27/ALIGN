f54a2f764db63bcead747f61c342334d5d41eaf2
Nhi Hoang <81710816+nhoangg27@users.noreply.github.com>
2026-05-13 14:54:38 -0700

    Fix formatting of CSV header in evaluate_DMS.py

--- graphormer/evaluate/evaluate_DMS.py
+++ graphormer/evaluate/evaluate_DMS.py
@@ -181,7 +181,7 @@ def eval(args, use_pretrained, checkpoint_path=None, logger=None):
               
                 with open(save, 'w', newline='') as file:
                     writer = csv.writer(file)
-                    writer.writerow(["SMILES", "Method", 'True CV', "Predicted CV", 'STD'])
+                    writer.writerow(["SMILES", "Method", "True CV", "Predicted CV", "STD"])
                     for row in stack:
                         writer.writerow(row)
                 print("SAVED PREDICTIONS")
