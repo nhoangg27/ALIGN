c0f7d85b0eead92f02befe394664b8d1e14e3122
Nhi Hoang <81710816+nhoangg27@users.noreply.github.com>
2026-05-13 14:54:52 -0700

    Fix CSV header row formatting in evaluate_GC.py

--- graphormer/evaluate/evaluate_GC.py
+++ graphormer/evaluate/evaluate_GC.py
@@ -188,7 +188,7 @@ def eval(args, use_pretrained, checkpoint_path=None, logger=None):
 
                 with open(save, 'w', newline='') as file:
                     writer = csv.writer(file)
-                    writer.writerow(["SMILES", "Method", 'True RI', "Predicted RI", 'STD'])
+                    writer.writerow(["SMILES", "Method", "True RI", "Predicted RI", "STD"])
                     for row in stack:
                         writer.writerow(row)
                 print("SAVED PREDICTIONS")
