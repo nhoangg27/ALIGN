dbe8f3221fa6d8a5053eadf0048c4b93370e592d
Nhi Hoang <81710816+nhoangg27@users.noreply.github.com>
2026-05-13 14:59:10 -0700

    Add evaluate_DMS.sh script for DMS evaluation

---
+++ graphormer/evaluate/evaluate_DMS.sh
@@ -0,0 +1,20 @@
+python evaluate_DMS.py \
+    --user-dir /workspaces/align/graphormer \
+    --num-workers 32 \
+    --ddp-backend=legacy_ddp \
+	--user-data-dir DMS_loader \
+	--dataset-name DMS_test \
+    --task graph_prediction \
+	--criterion rmse \
+	--arch graphormer_base \
+    --encoder-layers 8 \
+    --encoder-embed-dim  512 \
+    --encoder-ffn-embed-dim 512 \
+    --encoder-attention-heads 128 \
+    --freeze-level -4 \
+    --mlp-layers 5 \
+    --batch-size 64 \
+    --num-classes 1 \
+    --save-dir /workspaces/align/checkpoints/checkpoint_last.pt \
+    # --save-path /workspaces/align/results/DMS_sample.csv \
+    --split train \
